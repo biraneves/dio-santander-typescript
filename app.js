099d84826dcf8909b2c99ede3082b03a4c31c566
Ubirajara Neves <ubirajara.neves@usp.br>
2022-06-15 11:40:12 -0300

    Validações simples com TypeScript.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-let button = document.getElementById('button');
-let input1 = document.getElementById('input1');
-let input2 = document.getElementById('input2');
-
+"use strict";
+const button = document.getElementById('button');
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
 function somarNumeros(num1, num2) {
-    if (typeof(num1) === 'number' && typeof(num2) === 'number')
-        return num1 + num2;
-    else
-        return Number(num1) + Number(num2);
+    return num1 + num2;
 }
-
-button.addEventListener('click', () => {
-    console.log(somarNumeros(input1.value, input2.value));
-});
+if (button) {
+    button.addEventListener('click', () => {
+        if (input1 && input2) {
+            console.log(somarNumeros(Number(input1.value), Number(input2.value)));
+        }
+    });
+}
